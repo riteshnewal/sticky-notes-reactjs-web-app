fca229ea31711d2eb73f612bbace3bd2a7bd3446
kaushal shah <kaushal@infomaxdigital.com.au>
2022-12-30 11:04:54 +0530

    build

---
+++ build/precache-manifest.1e18fb70b6bc6e7c01b063354cc436ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e95f1e84b9b92b962e0",
+    "url": "/sticky-notes-reactjs-web/static/css/main.7e3ee699.chunk.css"
+  },
+  {
+    "revision": "6e95f1e84b9b92b962e0",
+    "url": "/sticky-notes-reactjs-web/static/js/main.b9104f1b.chunk.js"
+  },
+  {
+    "revision": "0fef34db8fd9fc957816",
+    "url": "/sticky-notes-reactjs-web/static/js/runtime~main.6962038f.js"
+  },
+  {
+    "revision": "d141ae339a223c37a6b6",
+    "url": "/sticky-notes-reactjs-web/static/css/2.ebe4c492.chunk.css"
+  },
+  {
+    "revision": "d141ae339a223c37a6b6",
+    "url": "/sticky-notes-reactjs-web/static/js/2.18291c01.chunk.js"
+  },
+  {
+    "revision": "6c101a14ef6ed4b025d5458f0a210229",
+    "url": "/sticky-notes-reactjs-web/index.html"
+  }
+];
